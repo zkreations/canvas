73b14c572e9d7b8ceff2337a312a2fb8f3a10259
Daniel Abel <daniel@zkreations.com>
2022-08-09 14:31:25 -0500

    build: basedir e ignorar guiones bajos

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const { src, dest, series, watch } = require('gulp')
 
 // Rutas
 const dir = {
+  allFolders: 'dev/**/',
   allFiles: 'dev/**/*',
   templates: 'dev/themes/*/*.pug',
   xml: 'dev/themes/*/*.xml'
@@ -12,9 +13,10 @@ const rename = require("gulp-rename")
 const pug = require('gulp-pug')
 
 const build = () => {
-  return src( [dir.templates], { base: "." } )
+  return src( [dir.templates, `!${dir.allFolders}_*`], { base: "." } )
     .pipe(pug({
       pretty: true,
+      basedir: "./dev/"
     }))
     .pipe( rename( { extname: '.xml' } ) )
     .pipe( dest( './' ) )
